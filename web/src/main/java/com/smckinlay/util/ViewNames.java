88a237f545dbd960c596b6ce655f002db178487a
Stewart <stewart.mckinlay@gmail.com>
2021-06-23 09:27:03 -0600

    Add thymeleaf and templates

---
+++ web/src/main/java/com/smckinlay/util/ViewNames.java
@@ -0,0 +1,12 @@
+package com.smckinlay.util;
+
+public final class ViewNames {
+
+    // == constants ==
+    public static final String HOME = "home";
+
+    // == constructors ==
+    private ViewNames() {
+    }
+
+}
