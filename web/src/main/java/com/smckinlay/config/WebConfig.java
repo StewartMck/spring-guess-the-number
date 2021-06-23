c211d7120205b1f43ad8ffdd4927907c6769ff73
Stewart <stewart.mckinlay@gmail.com>
2021-06-23 17:51:44 -0600

    Add interceptors and LocaleInterceptor

--- web/src/main/java/com/smckinlay/config/WebConfig.java
+++ web/src/main/java/com/smckinlay/config/WebConfig.java
@@ -1,15 +1,40 @@
 package com.smckinlay.config;
 
+import com.smckinlay.interceptor.RequestInterceptor;
 import com.smckinlay.util.ViewNames;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // == bean methods ==
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName(ViewNames.HOME);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
+
+        // this is for customizing the locale param: http://localhost:8080/?lang=es will display in spanish
+//        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+//        localeChangeInterceptor.setParamName("lang");
+//        registry.addInterceptor(localeChangeInterceptor);
+
+        // this uses the default implementation - locale
+        registry.addInterceptor(new LocaleChangeInterceptor());
     }
 }
---
+++ web/src/main/java/com/smckinlay/interceptor/RequestInterceptor.java
@@ -0,0 +1,35 @@
+package com.smckinlay.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class RequestInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.debug("prehandle method called={}", handler);
+        log.debug("URL={}", request.getRequestURL());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.debug("postHandle method called={}", handler);
+        log.debug("URL={}", request.getRequestURL());
+        log.debug("model={}", modelAndView.getModel());
+        log.debug("view={}", modelAndView.getView());
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.debug("afterCompletion method called={}", handler);
+        log.debug("URL={}", request.getRequestURL());
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
